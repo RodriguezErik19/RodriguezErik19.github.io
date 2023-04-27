8d37b1b60087e6372119a6730a7cce4dc1df6b89
RodriguezErik19 <124410825+RodriguezErik19@users.noreply.github.com>
2023-04-26 18:03:50 -0700

    Add files via upload
    base de datos de spotify ya terminado con relaciones y llena 26/04/23

---
+++ spotify.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-04-2023 a las 03:01:03
+-- Versión del servidor: 10.1.39-MariaDB
+-- Versión de PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `spotify`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `album_canciones`
+--
+
+CREATE TABLE `album_canciones` (
+  `id` int(11) NOT NULL,
+  `album_id` int(11) NOT NULL,
+  `cancion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `album_canciones`
+--
+
+INSERT INTO `album_canciones` (`id`, `album_id`, `cancion_id`) VALUES
+(1, 10, 7),
+(2, 9, 8);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `album_canciones`
+--
+ALTER TABLE `album_canciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `album_id` (`album_id`),
+  ADD KEY `cancion_id` (`cancion_id`) USING BTREE;
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `album_canciones`
+--
+ALTER TABLE `album_canciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `album_canciones`
+--
+ALTER TABLE `album_canciones`
+  ADD CONSTRAINT `album_canciones_ibfk_1` FOREIGN KEY (`cancion_id`) REFERENCES `canciones` (`id`),
+  ADD CONSTRAINT `album_canciones_ibfk_2` FOREIGN KEY (`album_id`) REFERENCES `albumes` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
