f553b2f07f1d1e04cc1725bcf73078936fa3174e
RodriguezErik19 <124410825+RodriguezErik19@users.noreply.github.com>
2023-05-22 17:58:45 -0700

    Add files via upload
    22/5/23

---
+++ parcial_3/practica1.php
@@ -0,0 +1,16 @@
+<?php
+echo "<h1>ola</h1> ";
+echo"<hr>";
+$nombre= "erik";
+$edad="17";
+if($edad >=18){
+    echo $nombre . " es mayor de edad";
+    } else{
+        echo $nombre . " es menor de edad";
+    }
+    echo"<br><br>";
+    for($i=0; $i<10; $i++){
+        echo $i . "<br>";
+    }
+
+?>
